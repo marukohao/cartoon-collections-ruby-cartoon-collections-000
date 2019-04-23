bd21ebf7aa6199aea772387300159b281b59d2aa
hao wang <ge_nah@hotmail.com>
2019-04-23 21:31:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,20 +1,40 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
-  dwarves.each_with_index { |word, i| puts "#{i + 1}.#{word}"}
+  new_dwarves = []
+  dwarves.each_with_index do |dwarf, i|
+    if i < dwarf.length / 2
+      new_dwarves << dwarf
+    end
+  end
+  puts new_dwarves
+  
+  i = 0
+  while i < dwarves.length /2 do
+    puts "#{i + 1} #{dwarves[i]}"
+  end
 end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
-  return planeteer_calls.collect { |word| word[0].upcase + word[1..-1] + "!" }
+  planeteer_calls.collect { |word| word[0].upcase + word[1..-1].downcase + "!" }
 end
 
 def long_planeteer_calls(short_words)# code an argument here
   # Your code here
-  return short_words.any? { |word| word.length > 4}
+  short_words.any? { |word| word.length > 4}
 end
 
 def find_the_cheese(food)# code an argument here
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  return food.find { |word| cheese_types.include?(word)}
+  cheese_types = ["cheddar", "gouda", "camembert", "swiss"]
+  food.find { |word| cheese_types.include?(word)}
 end
+
+
+def words_that_start_with_b(words)
+  words.select { |word| word[0] == "b"}
+end
+
+
+words_that_start_with_b(['banana', 'cherry', 'fries']) // ['banana']
+words_that_start_with_b(['brian', 'bobby', 'james']) // ['brian', 'bobby']
